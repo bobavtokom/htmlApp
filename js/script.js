0bcc65c0fbbefab52de48f6ea47bb2d97d30b227
Zoran Nakov <nakovz@yahoo.com>
2023-05-05 00:31:23 +0200

    Adding js logic to Like and Dislike button

--- js/script.js
+++ js/script.js
@@ -1,32 +1,51 @@
-const likeButton = document.getElementById('like-button');
-		const dislikeButton = document.getElementById('dislike-button');
-		const likeCount = document.getElementById('like-count');
-		const dislikeCount = document.getElementById('dislike-count');
+const LikeButtonClick = e => {
+	const articleElement = e.srcElement.parentElement;
+	const likeCountElement = articleElement.querySelector('#like-count');
+	const articleId = articleElement.getAttribute('data-article-id');
+	let storageKey = 'likes-'+articleId;
+	let storageVal = parseInt(localStorage.getItem(storageKey)) || 0;
 
+	storageVal++;
+	likeCountElement.textContent = storageVal;
+	localStorage.setItem(storageKey, storageVal);
+}
 
-		let currentLikes = parseInt(localStorage.getItem('likes')) || 0;
-		let currentDislikes = parseInt(localStorage.getItem('dislikes')) || 0;
-		likeCount.textContent = currentLikes;
-		dislikeCount.textContent = currentDislikes;
+const DislikeButtonClick = e => {
+	const articleElement = e.srcElement.parentElement;
+	const dislikeCountElement = articleElement.querySelector('#dislike-count');
+	const articleId = articleElement.getAttribute('data-article-id');
+	let storageKey = 'dislikes-'+articleId;
+	let storageVal = parseInt(localStorage.getItem(storageKey)) || 0;
 
-		likeButton.addEventListener('click', () => {
-		currentLikes++;
-		likeCount.textContent = currentLikes;
-		localStorage.setItem('likes', currentLikes);
-		});
+	storageVal++;
+	dislikeCountElement.textContent = storageVal;
+	localStorage.setItem(storageKey, storageVal);
+}
 
-		dislikeButton.addEventListener('click', () => {
-		currentDislikes++;
-		dislikeCount.textContent = currentDislikes;
-		localStorage.setItem('dislikes', currentDislikes);
-		});
-		
-	function openLeftSidebar() {
-		document.getElementById("sidebar").style.left = "0px";
-		// document.querySelector(".left-sidebar").style.left = "0";
-	}
-	function closeLeftSidebar() {
-		// document.querySelector(".left-sidebar").style.left = "-250px";
-		document.getElementById("sidebar").style.left = "-250px";
-	}
-	
+const InitLikesAndDislikes = articleElement => {
+	const articleId = articleElement.getAttribute('data-article-id');
+
+	const likeCountElement = articleElement.querySelector('#like-count');
+	let storageKey = 'likes-'+articleId;
+	let storageVal = parseInt(localStorage.getItem(storageKey)) || 0;
+	likeCountElement.textContent = storageVal;
+
+	const dislikeCountElement = articleElement.querySelector('#dislike-count');
+	storageKey = 'dislikes-'+articleId;
+	storageVal = parseInt(localStorage.getItem(storageKey)) || 0;
+	dislikeCountElement.textContent = storageVal;
+}
+
+document.querySelectorAll("#like-button").forEach(item => item.addEventListener("click", LikeButtonClick));
+document.querySelectorAll("#dislike-button").forEach(item => item.addEventListener("click", DislikeButtonClick));
+
+document.querySelectorAll(".article-world").forEach(article => InitLikesAndDislikes(article));
+
+function openLeftSidebar() {
+	document.getElementById("sidebar").style.left = "0px";
+	// document.querySelector(".left-sidebar").style.left = "0";
+}
+function closeLeftSidebar() {
+	// document.querySelector(".left-sidebar").style.left = "-250px";
+	document.getElementById("sidebar").style.left = "-250px";
+}
