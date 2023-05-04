7fd007dbd1245e93565b1b78af0150a1d4efe3a8
Boban Milosevik <bobavtokom888@gmail.com>
2023-05-04 19:35:00 +0200

    class time

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-// Git exercises
+const likeButton = document.getElementById('like-button');
+		const dislikeButton = document.getElementById('dislike-button');
+		const likeCount = document.getElementById('like-count');
+		const dislikeCount = document.getElementById('dislike-count');
+
+
+		let currentLikes = parseInt(localStorage.getItem('likes')) || 0;
+		let currentDislikes = parseInt(localStorage.getItem('dislikes')) || 0;
+		likeCount.textContent = currentLikes;
+		dislikeCount.textContent = currentDislikes;
+
+		likeButton.addEventListener('click', () => {
+		currentLikes++;
+		likeCount.textContent = currentLikes;
+		localStorage.setItem('likes', currentLikes);
+		});
+
+		dislikeButton.addEventListener('click', () => {
+		currentDislikes++;
+		dislikeCount.textContent = currentDislikes;
+		localStorage.setItem('dislikes', currentDislikes);
+		});
+				function openLeftSidebar() {
+		document.getElementById("left-sidebar").style.left = "0";
+		}
+		function closeLeftSidebar() {
+			document.getElementById("left-sidebar").style.left="-250px";
+		}
+        
